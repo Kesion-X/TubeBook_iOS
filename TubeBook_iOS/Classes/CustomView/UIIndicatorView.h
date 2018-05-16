c6474870c385b785897325285320d6b504de5c9b
kejianfang <kejianfang@meituan.com>
2018-05-16 15:13:03 +0800

    增加买点

--- TubeBook_iOS/Classes/CustomView/UIIndicatorView.h
+++ TubeBook_iOS/Classes/CustomView/UIIndicatorView.h
@@ -21,35 +21,76 @@ typedef NS_ENUM(NSInteger, UIIndicatorViewStyle) {
 
 @interface UIIndicatorView : UIScrollView
 
-@property (nonatomic, strong) UIView *indicatorView;
-@property (nonatomic, strong) UIColor *indicatorColor;//滑块背景
-@property (nonatomic) NSUInteger currentIndicator;//当前滑块位置
+@property (nonatomic, strong) UIView *indicatorView; // 滑块
+@property (nonatomic, strong) UIColor *indicatorColor; // 滑块背景
+@property (nonatomic) NSUInteger currentIndicator; // 当前滑块位置
 @property (nonatomic,strong) NSMutableArray *itemArrays;
 @property (nonatomic) UIIndicatorViewStyle style;
-@property (nonatomic, strong) id<UIIndicatorViewDelegate> delegate;
+@property (nonatomic, strong) id<UIIndicatorViewDelegate> indicatorDelegate;
 
-- (instancetype)initUIIndicatorViewWithFrame:(CGRect)frame style:(UIIndicatorViewStyle)style arrays:(NSMutableArray *)arrays;
 
-- (instancetype)initUIIndicatorView:(UIColor *)indicatorColor frame:(CGRect)frame arrays:(NSMutableArray *)arrays font:(UIFont *)font textNormalColor:(UIColor *)textNormalColor textLightColor:(UIColor *)textLightColor;
 
-- (instancetype)initUIIndicatorView:(UIColor *)indicatorColor style:(UIIndicatorViewStyle)style arrays:(NSMutableArray *)arrays font:(UIFont *)font textNormalColor:(UIColor *)textNormalColor textLightColor:(UIColor *)textLightColor isEnableAutoScroll:(BOOL)isEnableAutoScroll;
 
-- (instancetype)initUIIndicatorView:(UIColor *)indicatorColor font:(UIFont *)font;
+/**
+ * @brief 使用默认颜色，默认样式，建立一个indicator，可以使用自定布局改变布局大小
+ * @param font 字体大小
+ */
+- (instancetype)initUIIndicatorViewWithIndicatorColor:(UIColor *)indicatorColor font:(UIFont *)font;
+
+/**
+ * @brief 使用默认颜色，建立一个indicator，可以使用自定布局改变布局大小
+ * @param frame 布局
+ * @param style 样式
+ * @param arrays 字符串数组
+ */
+- (instancetype)initUIIndicatorViewWithFrame:(CGRect)frame style:(UIIndicatorViewStyle)style arrays:(NSArray *)arrays;
+
+- (instancetype)initUIIndicatorViewWithIndicatorColor:(UIColor *)indicatorColor
+                                                style:(UIIndicatorViewStyle)style
+                                               arrays:(NSArray *)arrays
+                                                 font:(UIFont *)font
+                                      textNormalColor:(UIColor *)textNormalColor
+                                       textLightColor:(UIColor *)textLightColor
+                                   isEnableAutoScroll:(BOOL)isEnableAutoScroll;
 
 - (instancetype)initUIIndicatorViewWithFrame:(CGRect)frame
                               indicatorColor:(UIColor *)indicatorColor
                                        style:(UIIndicatorViewStyle)style
-                                      arrays:(NSMutableArray *)arrays
+                                      arrays:(NSArray *)arrays
                                         font:(UIFont *)font
                              textNormalColor:(UIColor *)textNormalColor
                               textLightColor:(UIColor *)textLightColor
                           isEnableAutoScroll:(BOOL)isEnableAutoScroll;
 
-
-- (void)addIndicatorItemByString:(NSString *)item;
+/**
+ * @brief 获取以当前字体为准的ui高
+ */
 - (CGFloat)getUIHeight;
+
+/**
+ * @brief 获取当前控件的内容宽度
+ */
 - (CGFloat)getUIWidth;
+
+/**
+ * @brief 添加一个tab
+ * @param item indicator名
+ */
+- (void)addIndicatorItemByString:(NSString *)item;
+
+/**
+ * @brief 展现某个位置的indicator
+ * @param index 索引位置
+ */
 - (void)setShowIndicatorItem:(NSUInteger)index;
+
+/**
+ * @brief 如果你使用UIViewPage,可以将此方法放在 delegate的 scrollViewDidScroll:(UIScrollView *)scrollView 中进行监听
+ * @param scrollView scrollViewDidScroll:(UIScrollView *)scrollView
+ */
+- (void)changeIndicatorIndexByScrollerView:(UIScrollView *)scrollView;
+
+
 - (void)changeIndicatorViewSize:(BOOL)isRight scale:(CGFloat)scale;
 
 @end
